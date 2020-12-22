8551dc7dc8035b80410cf7ab9fb1fb1895e6a7e8
Pliavi <pliavi.rpgm@gmail.com>
2020-12-22 14:29:43 -0300

    starting app before add typescript

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="(giveaway, key) in giveaways" :key="key">
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import api from "./util/api";
+import { onMounted, ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  setup() {
+    const giveaways = ref([]);
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    onMounted(async () => {
+      const response = await api.get("/giveaways");
+      giveaways.value = response.data;
+    });
+
+    return {
+      giveaways,
+    };
+  },
+};
+</script>
