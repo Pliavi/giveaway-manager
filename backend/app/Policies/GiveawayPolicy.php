61d86fe637f0078fd1006d9ba08a34ad9a6ea8de
Pliavi <pliavi.rpgm@gmail.com>
2021-01-28 16:02:13 -0300

    remove unused reflection (log problem)

--- backend/app/Policies/GiveawayPolicy.php
+++ backend/app/Policies/GiveawayPolicy.php
@@ -5,7 +5,6 @@ namespace App\Policies;
 use App\Models\Giveaway;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use phpDocumentor\Reflection\Types\Boolean;
 
 class GiveawayPolicy
 {
