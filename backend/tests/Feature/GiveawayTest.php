c8005680750240ef05a6c1d8def0b37c1bd0fcf1
Pliavi <pliavi.rpgm@gmail.com>
2021-02-03 11:29:55 -0300

    'finish' giveaway test

--- backend/tests/Feature/GiveawayTest.php
+++ backend/tests/Feature/GiveawayTest.php
@@ -2,48 +2,169 @@
 
 namespace Tests\Feature;
 
+use App\Models\FollowingAccount;
 use App\Models\Giveaway;
-use App\Models\User;
+use App\Models\SocialNetwork;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class GiveawayTest extends TestCase
 {
+
     use RefreshDatabase;
+    use WithFaker;
 
-    private $user;
-
-    public function setUp(): void
+    /**
+     * @test
+     */
+    public function it_successfully_create_a_giveaway()
     {
-        parent::setUp();
-        $this->seed();
+        $this->withoutExceptionHandling();
 
-        $this->user = User::all()->random();
-    }
+        /**
+         * @var FollowingAccount
+         */
+        $followingAccount = FollowingAccount::factory()->create();
 
-    public function testPostGiveaway()
-    {
-        $response = $this->actingAs($this->user)->json('POST', '/api/giveaways', [
-            "name" =>  "teste",
-            "link" =>  "teste",
-            "description" =>  "teste",
+        $giveawayData = [
+            "name" =>  "test name",
+            "link" =>  "http://www.test.com/giveaway",
+            "description" =>  "a test description",
             "finish_date" =>  "2012-12-11",
             "following_accounts"  =>  [
                 [
-                    "username" =>  "teste_user",
-                    "social_network_id" =>  1
+                    "username" =>  $followingAccount->username,
+                    "social_network_id" =>  $followingAccount->socialNetwork->id
                 ]
             ]
-        ]);
+        ];
 
-        $response->assertStatus(200);
+        $this->actingAs($this->loggedInUser)
+            ->postJson('/api/giveaway', $giveawayData)
+            ->assertStatus(201)
+            ->assertJson(
+                collect($giveawayData)
+                    ->except(["finish_date"])
+                    ->toArray()
+            );
     }
 
-    public function testDeleteGiveaway()
+    /**
+     * @test
+     */
+    public function it_successfully_update_a_giveaway()
     {
-        $response = $this->actingAs($this->user)->json('DELETE', '/api/giveaways/1');
+        /** @var Giveaway */
+        $giveaway = $this->loggedInUser
+            ->giveaways()
+            ->save(Giveaway::factory()->make());
+        $socialNetwork = SocialNetwork::factory()->create();
 
-        $response->assertStatus(404);
+        $oldFollowingAccount = FollowingAccount::factory()->create();
+        $giveaway->followingAccounts()->attach($oldFollowingAccount);
+
+        $giveawayUpdateData = [
+            "name" =>  "updated name",
+            "finish_date" =>  "2014-11-12",
+            "added_following_accounts" => [
+                [
+                    "username" => $this->faker->userName,
+                    "social_network_id" => $socialNetwork->id
+                ]
+            ],
+            "removed_following_accounts" => [$oldFollowingAccount->id]
+        ];
+
+        $this->actingAs($this->loggedInUser)
+            ->putJson("/api/giveaway/{$giveaway->id}", $giveawayUpdateData)
+            ->assertStatus(200)
+            ->assertJson(
+                collect($giveawayUpdateData)
+                    ->except(
+                        [
+                            "finish_date",
+                            "added_following_accounts",
+                            "removed_following_accounts"
+                        ]
+                    )
+                    ->toArray()
+            );
+    }
+    /** @test */
+    public function it_unauthorize_and_fail_to_update_another_user_giveaway()
+    {
+        /** @var Giveaway */
+        $giveaway = $this->simpleUser
+            ->giveaways()
+            ->save(Giveaway::factory()->make());
+
+        $giveawayUpdateData = [
+            "name" =>  "will not update name",
+        ];
+
+        $this->actingAs($this->loggedInUser)
+            ->putJson("/api/giveaway/{$giveaway->id}", $giveawayUpdateData)
+            ->dump()
+            ->assertStatus(403);
+    }
+
+    /**
+     * @test
+     */
+    public function it_successfully_deletes_a_giveaway()
+    {
+        $giveaway = $this->loggedInUser
+            ->giveaways()
+            ->save(Giveaway::factory()->make());
+
+        $this->actingAs($this->loggedInUser)
+            ->delete("/api/giveaway/{$giveaway->id}")
+            ->assertSuccessful();
+
+        $this->expectException(ModelNotFoundException::class);
+        $giveaway->refresh();
+    }
+
+    /**
+     * @test
+     */
+    public function it_successfully_shows_a_giveaway()
+    {
+        $giveaway = $this->loggedInUser
+            ->giveaways()
+            ->save(Giveaway::factory()->make());
+
+        $this->actingAs($this->loggedInUser)
+            ->get("/api/giveaway/{$giveaway->id}")
+            ->assertSuccessful()
+            ->assertJson($giveaway->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function it_successfully_shows_all_user_giveaways()
+    {
+        $this->loggedInUser
+            ->giveaways()
+            ->saveMany(Giveaway::factory(3)->make());
+        $this->loggedInUser
+            ->giveaways()
+            ->saveMany(Giveaway::factory(2)->make([
+                "finish_date" => Carbon::now()->subDay()
+            ]));
+
+        $this->actingAs($this->loggedInUser)
+            ->get("/api/giveaway")
+            ->assertSuccessful()
+            ->assertJsonStructure([
+                "finished",
+                "running"
+            ])
+            ->assertJsonCount(2, "finished")
+            ->assertJsonCount(3, "running");
     }
 }
