2a42e8b6bda8ef5e69870719b5239ef456bca6a4
Pliavi <pliavi.rpgm@gmail.com>
2021-02-03 13:05:38 -0300

    add users to testcase

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected User $loggedInUser;
+    protected User $simpleUser;
+
+    public function setup(): void
+    {
+        parent::setup();
+
+        $users = User::factory(2)->create();
+        $this->loggedInUser = $users[0];
+        $this->simpleUser = $users[1];
+    }
 }
